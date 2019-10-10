ee8efd441375794587bb8987d9949063921f9361
Whu-wxy <641645560@qq.com>
2019-10-10 22:42:11 +0800

    fix angle bug,add kernel visualization

--- imgutils.cpp
+++ imgutils.cpp
@@ -197,11 +197,13 @@ Mat screen0(Mat src)
             else
                 img.at<char>(i, j) = 255;
 
+//            img.at<char>(i, j) = kernel.at<uchar>(k,l);
+
         }
     }
 
 
-    Mat roi = img(Rect(12*50, 12*50, 12, 12));
+    Mat roi = img(Rect(12*50, 12*50, 24, 24));
     return img;
 }
 
@@ -237,47 +239,13 @@ Mat screen45(Mat src)
             else
                 img.at<char>(i, j) = 255;
 
-        }
-    }
-
-
-    Mat roi = img(Rect(12*50, 12*50, 12, 12));
-    return img;
-}
-
-
-Mat screen15(Mat src)
-{
-    if(src.channels() != 1)
-        cvtColor(src, src, CV_RGB2GRAY);
-
-    Mat img(src.cols*12, src.rows*12, CV_8UC1, Scalar(255, 255, 255));
-    Mat kernel = (Mat_<uchar>(3, 51) << 153,148,120,77,53,28,26,60,87,122,131,135,132,124,116,104,73,47,23,6,56,66,85,57,51,39,
-                  19,8,15,2,7,17,55,79,83,99,102,109,112,117,105,74,54,14,24,64,84,121,137,142,150,
-                  145,139,101,69,48,11,34,68,100,128,138,143,147,141,125,97,71,43,13,30,62,90,107,110,96,91,
-                  76,52,27,20,5,4,21,25,37,45,82,92,94,95,98,63,41,1,38,67,89,127,134,140,149,
-                  136,126,88,59,31,12,46,75,114,130,146,151,152,144,136,86,61,40,18,49,70,103,119,123,115,111,
-                  108,93,80,65,36,3,22,50,35,9,16,32,44,81,78,58,29,10,42,72,106,113,118,129,133);
-
-    int q=4;
-    for(int i=0; i<img.rows; i++)
-    {
-        int k = i % kernel.rows;
-        int t = img.cols-(q*kernel.rows*(i/kernel.rows)) % img.cols;
-        for(int j=0; j<img.cols; j++)
-        {
-            int l = (j%img.cols + t) % kernel.cols;
-            int pixelValue = ceil(src.at<uchar>(i/12, j/12) / 255.0*153);
-            if(pixelValue < kernel.at<uchar>(k,l))
-                img.at<char>(i, j) = 0;
-            else
-                img.at<char>(i, j) = 255;
+//            img.at<char>(i, j) = kernel.at<uchar>(k,l);
 
         }
     }
 
 
-    Mat roi = img(Rect(12*50, 12*50, 12, 12));
+    Mat roi = img(Rect(12*50, 12*50, 24, 24));
     return img;
 }
 
@@ -303,16 +271,56 @@ Mat screen75(Mat src)
         for(int j=0; j<img.cols; j++)
         {
             int l = (j%img.cols + t) % kernel.cols;
+//            int pixelValue = ceil(src.at<uchar>(i/12, j/12) / 255.0*153);
+//            if(pixelValue < kernel.at<uchar>(k,l))
+//                img.at<char>(i, j) = 0;
+//            else
+//                img.at<char>(i, j) = 255;
+
+            img.at<char>(i, j) = kernel.at<uchar>(k,l);
+
+        }
+    }
+
+
+    Mat roi = img(Rect(12*50, 12*50, 24, 24));
+    return roi;
+}
+
+
+Mat screen15(Mat src)
+{
+    if(src.channels() != 1)
+        cvtColor(src, src, CV_RGB2GRAY);
+
+    Mat img(src.cols*12, src.rows*12, CV_8UC1, Scalar(255, 255, 255));
+    Mat kernel = (Mat_<uchar>(3, 51) << 153,145,136,117,95,81,8,52,93,104,97,86,77,69,59,54,41,29,7,5,36,23,13,18,26,34,
+                  46,64,67,72,79,25,50,66,90,103,122,128,130,137,134,118,102,82,16,51,96,115,132,147,152,
+                  148,139,126,105,98,78,15,27,80,73,71,61,53,48,31,14,1,10,17,4,3,6,30,49,60,68,
+                  75,84,89,106,83,37,35,85,107,119,131,138,146,142,140,129,109,92,32,39,91,111,124,141,144,
+                  120,101,88,74,63,58,2,20,65,47,43,40,28,11,12,24,38,42,55,21,22,56,62,70,87,100,
+                  114,121,127,113,99,45,9,57,110,123,135,143,151,150,149,133,112,94,44,19,76,108,116,125,136);
+
+    int q=4;
+    for(int i=0; i<img.rows; i++)
+    {
+        int k = i % kernel.rows;
+        int t = q*kernel.rows*(i/kernel.rows) % img.cols;
+        for(int j=0; j<img.cols; j++)
+        {
+            int l = (j%img.cols + t) % kernel.cols;
             int pixelValue = ceil(src.at<uchar>(i/12, j/12) / 255.0*153);
             if(pixelValue < kernel.at<uchar>(k,l))
                 img.at<char>(i, j) = 0;
             else
                 img.at<char>(i, j) = 255;
 
+//            img.at<char>(i, j) = kernel.at<uchar>(k,l);
+
         }
     }
 
 
-    Mat roi = img(Rect(12*50, 12*50, 12, 12));
+    Mat roi = img(Rect(12*50, 12*50, 24, 24));
     return img;
 }
